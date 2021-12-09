10932424fe06d49e8d429b9e771fca75a0022187
SebastianRuiz18 <89488090+SebastianRuiz18@users.noreply.github.com>
2021-12-08 23:45:31 -0800

    Update tipo8.s

--- Cap2/tipo8.s
+++ Cap2/tipo8.s
@@ -6,28 +6,28 @@
         var1:  .asciz  "%d\n"
 .text
 .global _start
-/* Salvo registros */
+
 _start: push { r4, lr }
-/* Primera llamada abs (1) */
+
         mov r0, # 1
         bl abs
-/* Imprimo primera llamada */
+
         mov r1, r0
         ldr r0, = var1
         bl printf
-/* Segunda llamada abs (-2) e imprimo */
+
         mov r0, # -2    
         bl abs
         mov r1, r0
         ldr r0, = var1
         bl printf
-/* Tercera llamada abs (3) e imprimo */
+
         mov r0, # 3
         bl abs
         mov r1, r0
         ldr r0, = var1
         bl printf
-/* Cuarta llamada abs ( -4) e imprimo */
+
         mov r0, # -4
         bl abs
         mov r1, r0
